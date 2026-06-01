8b6fc32a0328e4690e9376d0f9446e6b3a8be040
Void Stem <voidstem@gmail.com>
2026-06-01 18:46:55 +0200

    Create rip.sh

---
+++ rip.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#Made By Void Stem
+
+#Download/RIP
+OUTDIR="/storage/emulated/0/Download/RIP"
+mkdir -p "$OUTDIR"
+
+echo "===================="
+echo " RIP TOOL "
+echo "===================="
+
+echo "Enter video URL:"
+read URL
+
+if [ -z "$URL" ]; then
+  echo "No URL entered, Exiting!"
+  exit 1
+fi
+
+echo "Downloading and merging audio + video..."
+
+yt-dlp \
+  -f "bv*+ba/b" \
+  --merge-output-format mp4 \
+  --remux-video mp4 \
+  --no-playlist \
+  -o "$OUTDIR/%(title)s.%(ext)s" \
+  "$URL"
+
+echo ""
+echo "Done!"
+echo "Saved to: Download/RIP"
