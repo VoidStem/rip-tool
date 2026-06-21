e59b1eee85c93153887f5f1ff1bf1d9b2ae082f8
Void Stem <voidstem@gmail.com>
2026-06-21 12:09:03 +0200

    Update arch-install.sh

--- installers/arch-install.sh
+++ installers/arch-install.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Made by Void Stem
 set -e
 
-echo "===================="
-echo " RIP INSTALLER "
-echo "===================="
+clear
+echo "RIP INSTALLER"
+echo
 
-echo "[*] Updating packages..."
+echo "Updating packages..."
 sudo pacman -Sy
 
-echo "[*] Installing dependencies..."
-sudo pacman -S --needed --noconfirm python ffmpeg yt-dlp curl
+echo "Installing dependencies..."
+sudo pacman -S --needed --noconfirm ffmpeg yt-dlp curl
 
-echo "[*] Downloading rip.sh script from GitHub..."
+echo "Downloading rip.sh script from GitHub..."
 curl -L -o rip.sh https://raw.githubusercontent.com/VoidStem/rip-tool/refs/heads/main/rip.sh
 
-echo "[*] Installing RIP to /usr/local/bin/rip..."
+echo "Installing RIP to /usr/local/bin/rip..."
 chmod +x rip.sh
 sudo mv rip.sh /usr/local/bin/rip
 
-echo "[+] Installation complete!"
+echo "Installation complete!"
 echo "Run RIP with: rip"
