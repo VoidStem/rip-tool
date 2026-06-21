c3fd9b7d6aedf3a14a2662a3ae6ad35a9ff99631
Void Stem <voidstem@gmail.com>
2026-06-21 12:06:48 +0200

    Update termux-install.sh

--- installers/termux-install.sh
+++ installers/termux-install.sh
@@ -1,29 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Made by Void Stem
 set -e
 
-echo "===================="
-echo " RIP INSTALLER "
-echo "===================="
+clear
+echo "RIP INSTALLER"
+echo 
 
-echo "[*] Updating packages..."
+echo "Updating packages..."
 pkg update -y
 
-echo "[*] Installing dependencies..."
+echo "Installing dependencies..."
 pkg install -y python ffmpeg
 
-echo "[*] Installing yt-dlp with pip..."
+echo "Installing yt-dlp with pip..."
 pip install -U yt-dlp
 
-echo "[*] Setting up termux storage..."
+echo "Setting up termux storage..."
 termux-setup-storage
 mkdir -p $PREFIX/bin/
-echo "[*] Downloading RIP script from GitHub..."
+echo "Downloading RIP script from GitHub..."
 curl -L -o rip.sh https://raw.githubusercontent.com/VoidStem/rip-tool/refs/heads/main/termux-rip.sh
 
-echo "[*] Installing RIP to $PREFIX/bin/rip..."
+echo "Installing RIP to $PREFIX/bin/rip..."
 chmod +x rip.sh
 mv rip.sh $PREFIX/bin/rip
 
-echo "[+] Installation complete!"
+echo "Installation complete!"
 echo "To run RIP type: rip"
