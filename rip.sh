f08e5422d639b87ffb628a876d0de6c1671e0525
Void Stem <voidstem@gmail.com>
2026-06-21 12:15:56 +0200

    Update rip.sh

--- rip.sh
+++ rip.sh
@@ -1,36 +1,61 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Made By Void Stem
-set -e
 
-# Download/RIP
 OUTDIR="$HOME/Downloads/RIP"
 mkdir -p "$OUTDIR"
 
 clear
-
-echo "===================="
-echo " RIP TOOL "
-echo "===================="
+echo "RIP TOOL"
 echo
 
-read -rp "Enter video URL: " URL
+echo "Enter a video URL, playlist URL, or search term:"
+read -r INPUT
 
-if [ -z "$URL" ]; then
-echo "No URL entered, exiting!"
+if [ -z "$INPUT" ]; then
+echo "No input entered. Exiting."
 exit 1
 fi
 
-echo
-echo "Downloading and merging audio + video..."
-
-yt-dlp 
--f "bv*+ba/b" 
---merge-output-format mp4 
---remux-video mp4 
---no-playlist 
--o "$OUTDIR/%(title)s.%(ext)s" 
-"$URL"
+#User download mode
+echo "Choose mode:"
+echo "1) Audio only"
+echo "2) Audio + Video"
+read -r MODE
 
 echo
-echo "[+] Done!"
+
+OUTPUT="-o $OUTDIR/%(title)s.%(ext)s"
+
+echo "Starting..."
+
+#Check if it look like a url
+if [[ "$INPUT" =~ ^https?:// ]]; then
+SOURCE="$INPUT"
+echo "URL detected"
+else
+SOURCE="ytsearch1:$INPUT"
+echo "Search detected (YouTube only)"
+fi
+
+#Download audio (best quality)
+if [ "$MODE" == "1" ]; then
+echo "Mode: Audio only"
+yt-dlp \
+-x --audio-format mp3 \
+--audio-quality 0 \
+$OUTPUT \
+"$SOURCE"
+else
+
+#Download video + audio (best quality)
+echo "Mode: Audio + Video"
+yt-dlp \
+-f "bv*+ba/b" \
+--merge-output-format mp4 \
+$OUTPUT \
+"$SOURCE"
+fi
+
+echo
+echo "Done"
 echo "Saved to: $OUTDIR"
